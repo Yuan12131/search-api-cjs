453e962f02e1d56d536ed06a130ea323839ba402
leeyuan <dbdks32123@gmail.com>
2023-11-02 17:42:27 +0900

    서버코드 express, GETㄴ요청

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const { client_id, client_secret } = require('./mod/utils'); // 유틸리티 모듈 가져오기
+const { handleSearchRequest, handleRootRequest } = require('./mod/routes'); // 라우팅 핸들러 모듈 가져오기
+
+const port = 8080;
+
+// 정적 파일을 불러오기 위한 미들웨어 설정
+app.use(express.static('public'));
+
+// 루트 경로로 접근했을 때 index.html 파일 전송
+app.get('/', handleRootRequest); // 라우팅 핸들러로 변경
+
+app.get('/search/news', handleSearchRequest); // 라우팅 핸들러로 변경
+
+app.listen(port, function () {
+  console.log(`http://127.0.0.1:${port}`);
+});
